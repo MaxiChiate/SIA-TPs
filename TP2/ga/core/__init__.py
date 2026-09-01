ca117b7589f3832158d86747f451a6a2ddca3bb9
Máximo Chiatellino <mchiatellino@itba.edu.ar>
2026-09-01 15:10:46 -0300

    Add Gene and GeneSchema
    Co-Authored-By: Matías Coleur <matiascoleur02@gmail.com>
    Co-Authored-By: Gonzalo Sharif Curi Martínez <gonzalocuri@gmail.com>
    Co-Authored-By: Valentina Novillo <130627694+valenNovillo@users.noreply.github.com>
    Co-Authored-By: Magdalena Taurian <mtaurian@itba.edu.ar>

--- TP2/ga/core/__init__.py
+++ TP2/ga/core/__init__.py
@@ -2,9 +2,13 @@
 
 from __future__ import annotations
 
+from .gene import Gene, GeneKind, GeneSchema
 from .rng import Rng, make_rng
 
 __all__ = [
+    "Gene",
+    "GeneKind",
+    "GeneSchema",
     "Rng",
     "make_rng",
 ]
